f4af1e49caae16e20184a8c70a35bb04240e65cc
Sidney B <46009984+sidneybrownjr@users.noreply.github.com>
2022-03-01 15:09:53 -0600

    misc

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,5 +1,13 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+/*
+    Code 0: Success Returned results successfully.
+    Code 1: No Results Could not return results. The API doesn't have enough questions for your query. (Ex. Asking for 50 Questions in a Category that only has 20.)
+    Code 2: Invalid Parameter Contains an invalid parameter. Arguements passed in aren't valid. (Ex. Amount = Five)
+    Code 3: Token Not Found Session Token does not exist.
+    Code 4: Token Empty Session Token has returned all possible questions for the specified query. Resetting the Token is necessary.
+*/
+
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
