ed53d8851be06b8982a6c8795fd27019c0601f7f
Sidney B <46009984+sidneybrownjr@users.noreply.github.com>
2022-03-01 15:12:32 -0600

    Setup rough Results page

--- src/features/results/Results.jsx
+++ src/features/results/Results.jsx
@@ -1,3 +1,27 @@
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { changeScore } from "../options/optionsSlice";
+import { Stack, Heading, Button } from "@chakra-ui/react";
+
 export const Result = () => {
-  return <div>Results</div>;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { score } = useSelector((state) => state.options);
+
+  const returnToHome = () => {
+    dispatch(changeScore(0));
+    navigate("/");
+  };
+
+  return (
+    <Stack spacing={8}>
+      <Heading as="h1" color={"gray.50"}>
+        Final Score: {score}
+      </Heading>
+      <Button colorScheme="blue" onClick={() => returnToHome()} isFullWidth>
+        Return to Homepage
+      </Button>
+    </Stack>
+  );
 };
