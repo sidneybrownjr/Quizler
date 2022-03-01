163963ca84e07d6eb5912840016096d5f59234f2
Sidney B <46009984+sidneybrownjr@users.noreply.github.com>
2022-03-01 15:10:19 -0600

    Setup Homepage

--- src/features/options/Options.jsx
+++ src/features/options/Options.jsx
@@ -1,13 +1,15 @@
-import { Spinner } from "../../common/Spinner";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { useGetCategoriesQuery } from "../api/apiSlice";
-import {
-  changeCategory,
-  changeDifficulty,
-  changeType,
-  changeScore,
-} from "./optionsSlice";
+import { changeCategory, changeDifficulty, changeType } from "./optionsSlice";
+import { Box, Button, Spinner } from "@chakra-ui/react";
+import { Stack } from "@chakra-ui/react";
+import { SelectField } from "../../common/SelectField";
 
 export const Options = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   let content;
 
   // options for quiz "Difficulty"
@@ -30,43 +32,69 @@ export const Options = () => {
     error,
   } = useGetCategoriesQuery();
 
-  // define reusable handleChange to handle each select
-  let handleChange = (e) => {
-    console.log(e.target.name);
+  // uses the name of the node to dispatch the appropriate action
+  const handleChange = (e) => {
+    let nodeName = e.target.name;
+    switch (nodeName) {
+      case "category":
+        dispatch(changeCategory(e.target.value));
+        break;
+      case "difficulty":
+        dispatch(changeDifficulty(e.target.value));
+        break;
+      case "type":
+        dispatch(changeType(e.target.value));
+        break;
+      default:
+        return;
+    }
   };
 
   if (isLoading) {
-    content = <Spinner text="Loading..." />;
+    content = (
+      <Spinner
+        thickness="4px"
+        speed="0.60s"
+        emptyColor="gray.200"
+        color="blue.500"
+        boxSize={20}
+      />
+    );
   } else if (isSuccess) {
     content = (
-      <select
-        className="xl:w-full appearance-none rounded-large py-1 text-center"
-        defaultValue="DEFAULT"
-        name="category"
-        onChange={handleChange}
-      >
-        <option value="DEFAULT" disabled>
-          Select a Category
-        </option>
-        {categories?.map((category) => (
-          <option key={category.id} value={category.id}>
-            {category.name}
-          </option>
-        ))}
-      </select>
+      <>
+        <SelectField
+          label={"category"}
+          options={categories}
+          onChange={handleChange}
+        />
+        <SelectField
+          label={"difficulty"}
+          options={difficulty}
+          onChange={handleChange}
+        />
+        <SelectField label={"type"} options={types} onChange={handleChange} />
+        <Button
+          colorScheme="blue"
+          onClick={() => navigate("/quiz")}
+          isFullWidth
+        >
+          Begin Quiz
+        </Button>
+      </>
     );
   } else if (isError) {
     content = <div>{error}</div>;
   }
 
   return (
-    <div className="flex flex-col justify-center items-center h-full">
-      <div className="w-4/6 h-1/6 md:w-5/6 md:h-3/6 flex justify-center items-center">
-        <div className="items-center animate__animated animate__backInDown">
-          <img className="" src="../images/quizler_logo.png" alt="quizler" />
-        </div>
-      </div>
-      {content}
-    </div>
+    <Stack spacing={8} m={2}>
+      <Box>
+        <img className="" src="../images/quizler_logo.png" alt="quizler" />
+      </Box>
+      <Stack align="center" spacing={3} h={256}>
+        {content}
+      </Stack>
+    </Stack>
   );
 };
