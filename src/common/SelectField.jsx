3a1c34d57e87266ccded2028291a75a7da75c8c5
Sidney B <46009984+sidneybrownjr@users.noreply.github.com>
2022-03-01 21:30:13 -0600

    Added defaults for selects

--- src/common/SelectField.jsx
+++ src/common/SelectField.jsx
@@ -2,11 +2,16 @@ import { Select } from "@chakra-ui/react";
 
 export const SelectField = ({ label, options, onChange }) => (
   <Select
+    defaultValue={label}
     id={label}
     name={label}
+    color="gray.50"
     onChange={(e) => onChange(e)}
-    style={{ textAlign: "center", color: "#FEFEFE" }}
+    style={{ textAlign: "center", textTransform: "capitalize" }}
   >
+    <option value={label} disabled>
+      {label}
+    </option>
     {options?.map((option) => (
       <option key={option.id} value={option.id} style={{ color: "#000" }}>
         {option.name}
